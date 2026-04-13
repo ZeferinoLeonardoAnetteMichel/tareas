f4709b0c98fdf417804ec0a601e765e7942bf3f2
Zeferino Leonardo Anette Michel <23308060610457@cetis61.edu.mx>
2026-04-13 14:59:35 -0600

    cambios

--- src/main.py
+++ src/main.py
@@ -1,6 +1,26 @@
+import flet as ft 
+from controllers.UserController import AuthController
+from controllers.TareaController import TareaController
+from views.LoginView import LoginView
+from views.dashboard import DashboardView
+def start(page:ft.Page):
+    auth_ctrl=AuthController()
+    task_ctrl=TareaController()
+    def route_change(e):
+        page.views.clear()
+        if page.route == "/":
+            page.add(ft.Text("Caso1"))
+            page.views.append(LoginView(page, auth_ctrl))
+        elif page.route == "/dashboard":
+            page.views.append(DashboardView(page, task_ctrl))
+        if not page.views:
+            page:views.append(
+                ft.View("/",[ft.Text("Error:Ruta no encontrada o vista vacia")])
+                )
+        page.update()   
+    page.on_route_change=route_change
+    page.go("/")
 def main():
-    print("Hello from tareas!")
-
-
+    ft.app(target=start)
 if __name__ == "__main__":
     main()
