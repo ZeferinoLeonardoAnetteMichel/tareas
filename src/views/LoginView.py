4f636ffd1e897ba066a025efb7708170779d7154
ZeferinoLeonardoAnetteMichel <23308060610457@cetis61.edu.mx>
2026-04-21 07:23:13 -0500

    cambios

--- src/views/LoginView.py
+++ src/views/LoginView.py
@@ -1,28 +1,136 @@
 import flet as ft
 
-def LoginView(page: ft.Page, auth_controller):
-    email_input = ft.TextField(label="Correo electrónico", width=350, border_radius=10,keyboard_type=ft.KeyboardType.EMAIL)
-    pass_input = ft.TextField(label="Contraseña", password=True, can_reveal_password=True, width=350, border_radius=10)
+def main(page: ft.Page):
+    page.title = "Inicio de Sesión"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
-    def login_click(e):
-        user, msg = auth_controller.login(email_input.value, pass_input.value)
-        if user:
-            page.session.set("user", user) 
-            page.go("/dashboard")
+    titulo = ft.Text(
+        "Inicia sesión",
+        size=30,
+        weight="bold",
+        color="purple"
+    )
+
+    usuario = ft.TextField(
+    label="Usuario",
+    width=300,
+    border_color="purple",
+    icon=ft.Icons.PERSON
+)
+
+    password = ft.TextField(
+    label="Contraseña",
+    password=True,
+    can_reveal_password=True,
+    width=300,
+    border_color="purple",
+    icon=ft.Icons.LOCK
+)
+
+    mensaje = ft.Text()
+
+    def pantalla_principal():
+        page.controls.clear()
+        titulo_panel = ft.Container(
+            content=ft.Text(
+                "Panel Principal",
+                color="white",
+                size=30,
+                weight="bold"
+            ),
+            bgcolor="purple",
+            padding=20,
+            width=600
+        )
+
+        bienvenido = ft.Text(
+            "Bienvenido al Sistema",
+            size=40,
+            weight="bold"
+        )
+
+        subtexto = ft.Text(
+            "Has iniciado sesión correctamente",
+            size=20,
+            color="grey"
+        )
+        menu = ft.Row(
+            [
+                ft.Column(
+                    [
+                        ft.Icon(ft.Icons.HOME),
+                        ft.Text("Inicio")
+                    ],
+                    alignment=ft.MainAxisAlignment.CENTER,
+                    horizontal_alignment=ft.CrossAxisAlignment.CENTER
+                ),
+                ft.Column(
+                    [
+                        ft.Icon(ft.Icons.SEARCH),
+                        ft.Text("Explorar")
+                    ],
+                    alignment=ft.MainAxisAlignment.CENTER,
+                    horizontal_alignment=ft.CrossAxisAlignment.CENTER
+                ),
+                ft.Column(
+                    [
+                        ft.Icon(ft.Icons.PERSON),
+                        ft.Text("Perfil")
+                    ],
+                    alignment=ft.MainAxisAlignment.CENTER,
+                    horizontal_alignment=ft.CrossAxisAlignment.CENTER
+                ),
+            ],
+            alignment=ft.MainAxisAlignment.SPACE_AROUND,
+            width=300
+        )
+
+        page.add(
+            ft.Column(
+                [
+                    titulo_panel,
+                    ft.Container(height=60),
+                    bienvenido,
+                    subtexto,
+                    ft.Container(height=80),
+                    menu
+                ],
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER
+            )
+        )
+
+        page.update()
+
+    def login(e):
+        if usuario.value == "admin" and password.value == "1234":
+            mensaje.value = "Bienvenido"
+            mensaje.color = "green"
+            page.update()
+            pantalla_principal()
         else:
-            page.snack_bar = ft.SnackBar(ft.Text(msg))
-            page.snack_bar.open = True
+            mensaje.value = "Usuario o contraseña incorrectos"
+            mensaje.color = "red"
             page.update()
 
-    return ft.View("/", [
-        ft.AppBar(title=ft.Text("SIGE - Login"), bgcolor=ft.colors.BLUE_GREY_900, color="white"),
-        
-        ft.Column([
-            ft.Icon(ft.icons.LOCK_PERSON, size=50, color=ft.colors.BLUE),
-            ft.Text("Acceso al Sistema", size=20, weight="bold"),
-            email_input,
-            pass_input,
-            ft.ElevatedButton("Entrar", on_click=login_click, width=350),
-            ft.TextButton("Crear una cuenta nueva", on_click=lambda _: page.go("/registro"))
-        ], horizontal_alignment=ft.CrossAxisAlignment.CENTER, alignment=ft.MainAxisAlignment.CENTER)
-    ])
+    boton_login = ft.ElevatedButton(
+        "Iniciar sesión",
+        bgcolor="purple",
+        color="white",
+        on_click=login
+    )
+
+    page.add(
+        ft.Column(
+            [
+                titulo,
+                usuario,
+                password,
+                boton_login,
+                mensaje
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER
+        )
+    )
+
